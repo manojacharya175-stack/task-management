2ec8a2565c8da3a6d9e9642f7883491c1b2aa8ce
Manoj Kumar MS <250993613+manojacharya175-stack@users.noreply.github.com>
2025-12-23 09:15:36 +0530

    Added swagger

---
+++ src/TaskManagement.API/Controllers/TaskController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagement.Application.DTOs;
+using TaskManagement.Application.Interfaces;
+
+namespace TaskManagement.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TasksController : ControllerBase
+{
+    private readonly ITaskService _taskService;
+
+    public TasksController(ITaskService taskService)
+    {
+        _taskService = taskService;
+    }
+
+    // POST: api/tasks
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateTaskRequest request)
+    {
+        // Temporary userId (will come from JWT later)
+        var userId = Guid.NewGuid();
+
+        var result = await _taskService.CreateAsync(userId, request);
+        return Ok(result);
+    }
+
+    // GET: api/tasks
+    [HttpGet]
+    public async Task<IActionResult> GetMyTasks()
+    {
+        // Temporary userId
+        var userId = Guid.NewGuid();
+
+        var tasks = await _taskService.GetByUserAsync(userId);
+        return Ok(tasks);
+    }
+}
--- src/TaskManagement.API/Program.cs
+++ src/TaskManagement.API/Program.cs
@@ -14,18 +14,29 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+
 builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 builder.Services.AddScoped<ITaskService, TaskService>();
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 var summaries = new[]
 {
